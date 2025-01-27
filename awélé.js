537021a6dff7ad325caca6d2f1e79d923d51be44
Patrice976 <catinatp@gmail.com>
2025-01-27 10:45:26 +0100

    redesign de la classe plateau simplifié création fonction display soucis sur fonction isEmpty qui n'est pas considéré comme une fonction

--- awélé.js
+++ awélé.js
@@ -1,20 +1,30 @@
 class Plateau {
     constructor(){
-        this.zoneTerritoireSud = [A,B,C,D,E,F]
-        this.zoneTerritoireNord = [G,H,L,K,J,I];
+        this.case = {"A" : 1,
+                     "B" : 0,
+                     "C" : 2,
+                     "D" : 4,
+                     "E" : 1,
+                     "F" : 4,
+                     "G" : 5,
+                     "H" : 5,
+                     "I" : 1,
+                     "J" : 7, 
+                     "K" : 8,
+                     "L" : 2
 
-        this.territoireSud = new Map()
-        this.territoireNord = new Map()
+        }
 
-        for (let index = 0; index < 7; index++) {
-            this.territoireSud.set(zoneTerritoireSud[index], 0)
-            this.territoireNord.set(this.zoneTerritoireNord[index], 0)
-            
-         }
     };
 
     display () {
-        console.log (this.territoireSud, this.territoireNord)
+        console.log (this.case)
+    }
+
+    isEmpty () {
+        for (var i in this.case) {
+            this.case[i] = 0
+        }
     }
  
 
@@ -22,4 +32,11 @@ class Plateau {
 
 test = new Plateau()
 
-test.display 
+console.log("1er display")
+test.display()
+
+console.log("on reset le plateau")
+test.isEmpty()
+
+("second display")
+test.display()

4def74714650dd60cf95de4bd21989448bb73308
Patrice976 <catinatp@gmail.com>
2025-01-27 12:25:07 +0100

    fonction saw oppérationnelle

--- awélé.js
+++ awélé.js
@@ -16,14 +16,25 @@ class Plateau {
         }
 
         this.caseIndexNavigator = ['A','B','C','D','E','F','G','H','I','J','K','L']
+        this.indexDictionnary = {
+                     "A" : 0,
+                     "B" : 1,
+                     "C" : 2,
+                     "D" : 3,
+                     "E" : 4,
+                     "F" : 5,
+                     "G" : 6,
+                     "H" : 7,
+                     "I" : 8,
+                     "J" : 9, 
+                     "K" : 10,
+                     "L" : 11
+        }
 
     };
 
     display () {
         console.log (this.case)
-       
-
-
     };
 
     isEmpty () {
@@ -34,40 +45,38 @@ class Plateau {
         console.log("plateau reset");
     }
 
-    //saw(//combien? //zone?) { 
-        //stack de graine initialiser 
-        // retirer combien? à zone?
-        // ajouter combien? a stack de graine
-        // elenver 1 à stack de graine et ajouter 1 à zone? +1 
+    saw(number , place) { 
+        let i=1;
+        let sawStack = 0 //stack de graine initialiser 
+        this.case[place] -= number// retirer number à place
+        sawStack += number// ajouter number à sawStack
+       while (sawStack > 0) {
+        this.case[this.caseIndexNavigator[this.indexDictionnary[place]+i] ] +=1
+        sawStack -=1
+        i += 1
+       } // enlever 1 à sawstack et ajouter 1 à place +1 tant qu'il y a des graines 
+       this.display()
     }
  
 
 }
+
+class Joueur {
+    constructor(nom) {
+       this.nom = nom;
+       this.score = 0 
+   
+    }
+   
+    earn (x) {
+       this.score += x;
+       console.log(`${this.nom} à ${this.score} points`);
+    }
+   }
 
 test = new Plateau();
 
 console.log("1er display");
 test.display();
 
-console.log("on reset le plateau");
-test.isEmpty();
-
-console.log("2nd display");
-test.display();
-
-class Joueur {
- constructor(nom) {
-    this.nom = nom;
-    this.score = 0 
-
- }
-
- earn (x) {
-    this.score += x;
-    console.log(`${this.nom} à ${this.score} points`);
- }
-}
-
-patrice = new Joueur("Patrice")
-
-patrice.earn(6)
+test.saw(4,"A")
